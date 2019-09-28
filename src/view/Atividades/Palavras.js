ea368d6dd4dc37bdc9429df6232e6a68eba3f42e
Gabriela <gabrielafarias@alunos.utfpr.edu.br>
2019-09-28 18:44:58 -0300

    Implementado audios de cada silaba de b a h

--- src/view/Atividades/Palavras.js
+++ src/view/Atividades/Palavras.js
@@ -2,7 +2,13 @@ import React, { Component } from 'react';
 import ViewQuadro from './../../components/Quadro/ViewQuadro';
 import { withStyles } from '@material-ui/styles';
 import Sound from 'react-sound';
-import ba from '../../Audios/ba.mp3'
+import A from '../../Audios/a.mp3';
+import BA from '../../Audios/ba.mp3';
+import CA from '../../Audios/ca.mp3';
+import DA from '../../Audios/da.mp3';
+import FA from '../../Audios/fa.mp3';
+import GA from '../../Audios/ga.mp3';
+import HA from '../../Audios/ha.mp3';
 
 const style = ({
     view: {
@@ -21,6 +27,7 @@ const style = ({
 class Palavra extends Component {
     constructor(props){
         super(props);
+        window.soundManager.setup({ debugMode: false });
 
         this.state = {
             letraNivel: 'A',
@@ -30,6 +37,13 @@ class Palavra extends Component {
                 ['J', 'L', 'M', 'N', 'P', 'Q'],
                 ['R', 'S', 'T', 'V', 'X', 'Z']
             ], 
+            sound: A,
+            soundListA: [
+                [BA, CA, DA, FA, GA, HA],
+                ['J', 'L', 'M', 'N', 'P', 'Q'],
+                ['R', 'S', 'T', 'V', 'X', 'Z']
+            ],
+            soundList: [],
             indexNivelSilabas: 0,
             index: 0,
             play: Sound.status.PAUSED
@@ -40,11 +54,16 @@ class Palavra extends Component {
     }
 
     componentDidMount(){
-        console.log(this.props.match.params)
         var vogal = this.props.match.params.vogal.toUpperCase()
 
-        console.log(vogal)
+        var soundList = [];
+
+        if (this.props.match.params.vogal.toUpperCase() === 'A') {
+            soundList = this.state.soundListA
+        }
+
         this.setState({
+            soundList: soundList,
             letraNivel: vogal,
             indexNivelSilabas: parseFloat(this.props.match.params.index)
         })
@@ -53,8 +72,8 @@ class Palavra extends Component {
 
     clickProximo(){
         if (this.state.index <= 5) {
-            console.log(this.state.silabasUtilizadas)
             this.setState({
+                sound: this.state.soundList[this.state.indexNivelSilabas][this.state.index],
                 silaba: this.state.silabasUtilizadas[this.state.indexNivelSilabas][this.state.index] + this.state.letraNivel,
                 index: this.state.index + 1
             })
@@ -80,11 +99,15 @@ class Palavra extends Component {
                 {this.state.silaba}
                 </div>
                 <Sound
-                    url={ba}
+                    url={this.state.sound}
                     playStatus={this.state.play}
                     onLoading={() => {}}
                     onPlaying={() => {}}
-                    onFinishedPlaying={() => {}}
+                    onFinishedPlaying={() => {
+                            this.setState({
+                                play: Sound.status.PAUSED
+                            })
+                    }}
                 />
             </ViewQuadro>
         );
